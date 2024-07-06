954927a3725724f567044eeea4b7ce9baecc52ca
hayabusa888a <152775236+hayabusa888a@users.noreply.github.com>
2024-07-06 01:16:03 +0000

    [Finish] Bookers2-35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+  
   def index
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
 end
