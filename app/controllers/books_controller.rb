79a36eb08ead688ae27e7b86f20204063fc5a702
hayabusa888a <152775236+hayabusa888a@users.noreply.github.com>
2024-07-08 13:53:42 +0000

    [Finish] Bookers2-41

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
      if @book.save
-       redirect_to book_path(current_user.id)
+       redirect_to book_path(@book)
      else
        render :index
      end
@@ -15,10 +15,35 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @user = current_user
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @booknew = Book.new
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else 
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   
   private
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get '/home/about' => 'homes#about', as: 'about'
-  resources :books, only: [:new, :index, :show, :create ]
-  resources :users, only: [:index, :show, :edit, :update ]
+  resources :users, only: [:index, :show, :edit, :update] 
+  resources :books, only: [:new, :index, :show, :edit, :update, :create, :destroy]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
